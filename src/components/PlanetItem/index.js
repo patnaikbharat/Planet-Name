646059de88f85131dbf0f189a7eaa10c87172048
patnaikbharat <patnaikbharatkumar@gmail.com>
2024-02-11 09:49:39 +0000

    planet app

---
+++ src/components/PlanetItem/index.js
@@ -0,0 +1,16 @@
+import './index.css'
+
+const PlanetItem = props => {
+  const {planetDetails} = props
+  const {name, imageUrl, description} = planetDetails
+
+  return (
+    <div className="planet-container">
+      <img src={imageUrl} className="planet-image" alt={`planet ${name}`} />
+      <h1 className="planet-name">{name}</h1>
+      <p className="planet-description">{description}</p>
+    </div>
+  )
+}
+
+export default PlanetItem
---
+++ src/components/PlanetsSlider/index.js
@@ -0,0 +1,20 @@
+import Slider from 'react-slick'
+import PlanetItem from '../PlanetItem'
+import './index.css'
+
+const PlanetsSlider = props => {
+  const {planetsList} = props
+
+  return (
+    <div className="slider-container" data-testid="planets">
+      <h1 className="main-heading">PLANETS</h1>
+      <Slider>
+        {planetsList.map(eachPlanet => (
+          <PlanetItem key={eachPlanet.id} planetDetails={eachPlanet} />
+        ))}
+      </Slider>
+    </div>
+  )
+}
+
+export default PlanetsSlider
